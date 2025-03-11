84b4c042868e090ae88c6658b47edfdced60f777
irmasilvera609 <irmasilvera609@gmail.com>
2025-03-11 21:14:26 +0000

    Add 6.js

---
+++ src/6.js
@@ -0,0 +1,7 @@
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+var randomString = "The quick brown fox jumps over the lazy dog";
+var randomBoolean = Math.random() < 0.5;
+
+console.log("Random number: " + randomNumber);
+console.log("Random string: " + randomString);
+console.log("Random boolean: " + randomBoolean);
