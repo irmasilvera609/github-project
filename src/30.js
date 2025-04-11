bf814bc572cda463faae5896fc1bb17953bb39fe
irmasilvera609 <irmasilvera609@gmail.com>
2025-04-11 12:25:04 +0000

    Add 30.js

---
+++ src/30.js
@@ -0,0 +1,8 @@
+// This is a very basic example of a JavaScript function that adds two numbers.
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+// Test the function with some sample data points.
+console.log(addNumbers(5, 3)); // Output: 8
+console.log(addNumbers(7.5, 2.3)); // Output: 9.8
