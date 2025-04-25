2ce01ea8fad3de1e7db9314263e0bb55c371a8ec
irmasilvera609 <irmasilvera609@gmail.com>
2025-04-25 05:53:21 +0000

    Add 60.js

---
+++ src/60.js
@@ -0,0 +1,9 @@
+// This is just an example to demonstrate how you can use JavaScript.
+let name = "John Doe";
+console.log(`Hello, ${name}!`);
+
+function greetUser() {
+  console.log("Hello, user!");
+}
+
+greetUser();
